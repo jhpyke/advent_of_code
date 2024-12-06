f87d42efabf541778684ca00bbfd3e11c7902ac2
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2024-12-06 12:20:53 +0000

    Adding a little commentary
    Signed-off-by: Jake H-P <72385707+jhpyke@users.noreply.github.com>

--- 2024/01/script/solution.py
+++ 2024/01/script/solution.py
@@ -8,7 +8,7 @@ input_data = pd.read_csv('./2024/01/data/input.txt', header=None, sep="   ")
 def column_cleaner(input_column: DataFrame) -> DataFrame:
     clean_copy = input_column.copy()
     clean_copy.sort_values(ascending=True, inplace=True, ignore_index=True)
-    clean_copy.reset_index(drop=True)
+    clean_copy.reset_index(drop=True) # This is here to achieve what ignore_index=True should have done in principle, but hasn't
     return clean_copy
 
 first_list = column_cleaner(input_data[0])
@@ -21,11 +21,13 @@ total_diff = diff.sum()
 def similarity_scorer(first_list: DataFrame, second_list: DataFrame) -> int:
     frequency_dict = {}
     similarity_score = 0
+    # Second list we only care about the frequencies associated with each value
     for value in second_list:
         if value in frequency_dict:
             frequency_dict[value] += 1
         else:
-            frequency_dict[value] = 1
+            frequency_dict[value] = 1\
+    # First list we care about the value X the frequency of that value in the second list
     for value in first_list:
         if value in frequency_dict:
             similarity_score += (value*frequency_dict[value])
